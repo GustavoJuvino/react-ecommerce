abf5982128af3b84a38cf0e3aebedb4bdbdbf46f
GustavoSouza <juvinogustavo1@gmail.com>
2023-04-11 19:32:25 -0300

    Modal Buttons now can change the main image in modal section

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,8 +10,10 @@ type ActiveModal = {
 
 const Modal: React.FC<ActiveModal> = ({ modal, setModal }) => {
     const [src, setSrc] = useState<string>();
+    const [index, setIndex] = useState<number>(1);
     const modalRef = useRef(null);
 
+    // We can refactor
     useEffect(() => {
         let handler = (e: MouseEvent) => {
           if (!modalRef.current?.contains(e.target as Node)) {
@@ -25,50 +27,51 @@ const Modal: React.FC<ActiveModal> = ({ modal, setModal }) => {
           document.removeEventListener("mousedown", (e: MouseEvent) => handler(e));
     });
 
-    function imgsTest(){
-        
-    }
-
-  return (
-    <Main style={{ display: modal ? "block" : "none"}}>
-        {modal ? (
-            <ModalSection ref={modalRef}>
-                <CloseModal
-                    className="delete"
-                    onClick={() => setModal(false)}
-                />
+  return (<Main style={{ display: modal ? "block" : "none"}}>
+    {modal ? (
+        <ModalSection ref={modalRef}>
+            <CloseModal
+                className="delete"
+                onClick={() => setModal(false)}
+            />
                 
-                <div className="main-img-container">
-                    <div className="previous"> {" < "} </div>
-                    <img 
-                        alt="main-img"
-                        src={src ? src : require("../assets/image-product-1.jpg")}
-                    />
-                    <div 
-                        className="next"
-                        onClick={() => imgsTest()}
-                    >
-                        {" > "}
-                    </div>
+            <div className="main-img-container">
+                <div 
+                    className="previous"
+                    onClick={() => index > 1 ? setIndex(index - 1) : null}
+                > 
+                    {" < "}
                 </div>
+                <img 
+                    alt="main-img"
+                    src={src ? src : require(`../assets/image-product-${index}.jpg`)}
+                />
+                <div 
+                    className="next"
+                    onClick={() => index <= 3 ? setIndex(index + 1) : null}
+                >
+                    {" > "}
+                </div>
+            </div>
             
-                <ul>
-                    {images?.map((img, index) => (
-                        <li key={index}>
-                            <img
-                                alt={`img-${index}`}
-                                onClick={(event: React.MouseEvent<HTMLImageElement>) => {
-                                    setSrc(event.currentTarget.src)
-                                }}
-                                src={require(`../assets/image-${img}`)}
-                            />
-                            </li>
-                        ))}
-                </ul>
-                </ModalSection>
-        ) : null}
+            <ul>
+                {images?.map((img, index) => (
+                    <li key={index}>
+                        <img
+                            onClick={(event: React.MouseEvent<HTMLImageElement>) => {
+                                setSrc(event.currentTarget.src)
+                                setIndex(index);
+                            }}
+                            alt={`img-${index}`}
+                            src={require(`../assets/image-${img}`)}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </ModalSection>
+    ) : null}
     </Main>
   )
 }
 
-export default Modal
+export default Modal;
